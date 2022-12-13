32c26c82ef6f68fda4ff284eea2671d7a4ca7254
Ivan Kovač <ivan.kovac995@gmail.com>
2022-12-13 13:21:19 +0100

    React Project Excercise

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMetupsPage from "./pages/AllMetups";
+import NewMeetupsPage from "./pages/NewMetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupsPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
